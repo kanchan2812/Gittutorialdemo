669bde0bc03214ea2243e57028a73b235ac43154
kanchan2812 <dighekanchan4@gmail.com>
2023-07-27 14:06:51 +0530

    idontlove git

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 console.log("I love Git");
+
+//In this window will be our document.
+//1.<head> will be element contains child element <h1> which contains title.
+//our root element second branch will be <body> element which child element element will be <input> tag with attributes like "type" and "value".
+
