488d1596ce28c347f1d0d437a291219c3bb25644
kanchan2812 <dighekanchan4@gmail.com>
2023-08-07 13:21:06 +0530

    Modifying Dom

--- dom.js
+++ dom.js
@@ -1,18 +1,74 @@
+var itemList=document.querySelector('#items');
 
-//QUERYSELECTOR
-/*var secondItem=document.querySelector('.list-group-item:nth-child(2)');
-secondItem.style.color='green';
+//parentNode
+console.log(itemList.parentNode);
+itemList.parentNode.style.backgroundColor='#ccc';
+console.log(itemList.parentNode.parentNode.parentNode);
 
-var thirdItem=document.querySelector('.list-group-item:nth-child(3)');
-thirdItem.style.display='none';*/
 
-//QuerySelectorAll
-var odd=document.querySelectorAll('li:nth-child(odd)');
-for(var i=0;i<odd.length;i++){
-  odd[i].style.backgroundColor='green';
-}
+//parentElement
+console.log(itemList.parentElement);
+itemList.parentElement.style.backgroundColor='#ccc';
+console.log(itemList.parentElement.parentElement.parentElement);
 
-var secondItem=document.querySelectorAll('.list-group-item:nth-child(2)');
-for(var i=0;i<odd.length;i++){
-secondItem.style.color='green';
-}
+//childNodes
+console.log(itemList.childNodes);
+
+console.log(itemList.children);
+console.log(itemList.children[1]);
+itemList.children[1].style.backgroundColor='yellow'
+
+//firstChild
+console.log(itemList.firstChild);
+
+
+//firstElementChild
+console.log(itemList.firstElementChild);
+itemList.firstElementChild.textContent='Hello 1';
+
+//LastChild
+console.log(itemList.lastChild);
+
+//LastChildElement
+console.log(itemList.lastElementChild);
+itemList.lastElementChild.textContent='Hello 4';
+
+//nextSibling
+console.log(itemList.nextSibling);
+
+//nextElementSibling
+console.log(itemList.nextElementSibling);
+
+//previousSibling
+console.log(itemList.previousSibling);
+
+//previousElementSibling
+console.log(itemList.previousElementSibling);
+itemList.previousElementSibling.style.color='green';
+
+//CreateElement
+//Create div
+var newDiv=document.createElement('div');
+//Add Class
+newDiv.className='Hello';
+//Add id
+newDiv.id='Hello1';
+
+//Add Attr
+newDiv.setAttribute('title','Hello Div');
+
+//Create text Node
+var newDivText=document.createTextNode('Hello World');
+
+//Add text to div
+
+newDiv.appendChild(newDivText);
+
+
+var container =document.querySelector('header.container');
+var h1=document.querySelector('header h1');
+
+console.log(newDiv);
+newDiv.style.fontSize='30px';
+
+container.insertBefore(newDiv,h1);
